d55e2e3ac1ec9bd88c3c412136d7ef39e0b21758
davideprimiceri <davideprimiceri95@hotmail.it>
2019-02-12 00:16:51 +0100

    added temp model

--- EmoGest.py
+++ EmoGest.py
@@ -1,18 +1,22 @@
 import numpy as np
-# import keras
+import keras
 import cv2
+import scipy.misc
+import pandas as pd
 
-hand_cascade = cv2.CascadeClassifier('haarcascade/cascade.xml')
+#hand_cascade = cv2.CascadeClassifier('haarcascade/cascade4.xml')
+model = keras.models.load_model("gesture_recog_ONE-HOT.h5")
 
-
-# model = keras.models.load_model("gesture_recog.h5")
-
-def img_to_mnist(img):
-    new_img = cv2.resize(img, (28, 28))
+def img_to_mnist(img, i):
+    new_img = cv2.resize(img,(28,28))
     new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+    #scipy.misc.imsave('test/'+str(i)+'.jpg', new_img)
     new_img = new_img.reshape(1, 28, 28, 1)
     return new_img
-
+	
+def index_to_label(index):
+    labels = ['fist', 'stop', 'question', 'ok', 'loser']
+    return labels[index]
 
 cap = cv2.VideoCapture(0)
 
@@ -20,19 +24,19 @@ cap = cv2.VideoCapture(0)
 if (cap.isOpened() == False):
     print("Error opening video stream or file")
 
+i = 0
 # Read until video is completed
 while (cap.isOpened()):
+    i += 1
     # Capture frame-by-frame
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    hand = hand_cascade.detectMultiScale(gray, 1.3, 5)  # DETECTING HAND IN THE THRESHOLDE IMAGE)
-    for (x, y, w, h) in hand:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    # new_img = img_to_mnist(frame)
-    # new_img = new_img/255
-    # prediction = model.predict(new_img)
-    # if (prediction.max() >= 0.7):
-    #    print(prediction.argmax(),prediction.max())
+    new_img = img_to_mnist(frame, i)
+    new_img = new_img/255
+    prediction = model.predict(new_img)
+    if (prediction.max() >= 0.8):
+        print(prediction)
+        label = index_to_label(prediction.argmax())
+        print(label,prediction.max(),'\n')
     if ret == True:
 
         # Display the resulting frame
